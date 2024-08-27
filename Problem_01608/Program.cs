f352c5423a83fad7a932143dd4ab4c870539ad32
ryouji <helloryouji@outlook.com>
2024-08-27 16:48:11 +0800

    update namespace

---
+++ Problem_01608/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ThinkLeetCode.Problem_01608;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        if (Test()) Console.WriteLine("Pass");
+        else Console.WriteLine("Fail");
+    }
+
+    public static bool Test()
+    {
+        if (SpecialArray([3, 5]) != 2) return false;
+        if (SpecialArray([0, 0]) != 2) return false;
+        if (SpecialArray([0, 4, 3, 0, 4]) != 3) return false;
+        if (SpecialArray([3, 6, 7, 7, 0]) != 3) return false;
+
+        return true;
+    }
+
+    public static int SpecialArray(int[] nums)
+    {
+        throw new NotImplementedException();
+    }
+}
